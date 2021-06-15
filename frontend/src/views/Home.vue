b19e10b1c88584f4967782e60a32f23b7e05579e
class PythonAddict <classpythonaddike@gmail.com>
2021-06-15 14:04:24 +0530

    Removed vuetify

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
-  <hello-world />
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
   }
+}
 </script>
