92c4227decfb056b68d0063336745810e3424bf7
devnull03 <arnav2003mehta@gmail.com>
2021-06-22 14:43:29 +0530

    Created the recent books component for dashboard

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="dashboard" :style="cssVars">
+    <nav-bar :fixed="true" />
+    <recent-books :books="UserBooks" />
+  </div>
+</template>
+
+<script>
+import RecentBooks from "../components/RecentBooks.vue";
+import NavBar from "../components/NavBar.vue";
+
+export default {
+  name: "Dashboard",
+  components: {
+    RecentBooks,
+    NavBar,
+  },
+  data() {
+    return {
+      UserBooks: [
+        "booooooooooooooooooooooooooooooooooooook",
+        "book1",
+        "book2",
+        "book3",
+        "book4",
+        "book5",
+        "book",
+        "book",
+        "book",
+        "book",
+        "book",
+      ],
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
