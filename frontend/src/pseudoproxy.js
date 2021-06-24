216384f9c083df099115b2da5350d6ce6bbc5ad0
PythonAddict <classpythonaddike@gmail.com>
2021-06-24 15:09:32 +0530

    Removed pseudoproxy as well :facepalm:

--- frontend/src/pseudoproxy.js
+++
@@ -1,8 +0,0 @@
-class Proxy() {
-  constructor(url) {
-    this.url = url
-  },
-    fetch(route, then_function, error_function, request_body) {
-      return fetch(route, request_body).then(then_function).catch(error_function)
-    }
-}
