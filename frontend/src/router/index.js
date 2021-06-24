e6ac1c2641cdcef3bc7b3b889a177c44dd459f66
devnull03 <arnav2003mehta@gmail.com>
2021-06-24 10:45:51 +0530

    The previous 2 commits never happened :mhmf:

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import BookInfo from "../views/BookInfo.vue"
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/dev/book-info',
+    name: 'BookInfo',
+    component: BookInfo
   }
 ]
 
