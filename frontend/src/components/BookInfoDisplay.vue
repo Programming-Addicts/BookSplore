a6bfbd4c80f6e27cd620fbba91f20b834989bf26
devnull03 <arnav2003mehta@gmail.com>
2021-06-24 16:04:48 +0530

    Created html structure for book info

---
+++ frontend/src/components/BookInfoDisplay.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="bookInfoDisplay">
+        <img
+            :src="
+                bookData.cover !== null ? bookData.cover : require('../assets/ProfilePicture.svg')
+            "
+            class="cover"
+        />
+        <div class="info">
+            <div class="head">
+                <p class="title">{{ bookData.title }}</p>
+                <p class="belowTitle">
+                    Written by {{ bookData.author }} | Published in
+                    {{ bookData.published.getFullYear() }}
+                </p>
+            </div>
+            <div class="infoTable">
+                <div class="left">
+                    <div>
+                        <p>Published</p>
+                        {{
+                            bookData.published
+                                ? bookData.published.toDateString().slice(4)
+                                : "-"
+                        }}
+                    </div>
+                    <div>
+                        <p>Publisher</p>
+                        {{ bookData.publisher ? bookData.publisher : "-" }}
+                    </div>
+                    <div>
+                        <p>Page count</p>
+                        {{ bookData.pageCount ? bookData.pageCount : "-" }}
+                    </div>
+                    <div>
+                        <p>Page count</p>
+                        {{ bookData.language ? bookData.language : "-" }}
+                    </div>
+                </div>
+                <div class="right">
+                    <div><p>Ratings</p>{{bookData.ratings? bookData.ratings: "-"}} </div>
+                    <div><p>Ratings</p>{{bookData.avgRatings? bookData.avgRatings: "-"}} </div>
+                    <div><p>Ratings</p>{{bookData.reviewsAmount? bookData.reviewsAmount: "-"}} </div>
+                </div>
+            </div>
+        </div>
+        <div class="desc">{{ bookData.description }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BookInfoDisplay",
+    props: ["Book"],
+    data() {
+        return {
+            bookData: {
+                cover: this.Book.cover,
+                title: this.Book.title,
+                author: this.Book.author,
+                published: this.Book.published,
+                publisher: this.Book.publisher,
+                language: this.Book.language,
+                pageCount: this.Book.pageCount,
+                ratings: this.Book.ratings,
+                avgRating: this.Book.avgRating,
+                reviewsAmount: this.Book.reviewsAmount,
+                description: this.Book.desc
+            }
+        };
+    }
+};
+</script>
+
+<style scoped>
+.bookInfoDisplay {
+    display: grid;
+    position: relative;
+    grid-template-areas:
+        "cover info"
+        "desc desc";
+}
+.bookInfoDisplay img {
+    grid-area: "cover";
+}
+.info {
+    grid-area: "info";
+}
+.desc {
+    grid-area: "desc";
+}
+</style>
--- frontend/src/views/BookInfo.vue
+++ frontend/src/views/BookInfo.vue
@@ -1,14 +1,44 @@
 <template>
-  <div class="bookInfoMain">
-    book, yes
-  </div>
+    <div class="bookInfo">
+        <nav-bar :fixed="true" navbar_type="authenticated" />
+        <div class="bookInfoMain">
+            <book-info-display :Book="dummyData" />
+        </div>
+        <Footer></Footer>
+    </div>
 </template>
 
 <script>
+import NavBar from "../components/NavBar.vue";
+import Footer from "../components/Footer.vue";
+import BookInfoDisplay from "../components/BookInfoDisplay.vue";
+
 export default {
-  name: "BookInfo",
-  components: {},
+    name: "BookInfo",
+    components: {
+        NavBar,
+        Footer,
+        BookInfoDisplay
+    },
+    data() {
+        return {
+            dummyData: {
+                cover: null,
+                title: "the ballad of songbirds and snakes",
+                author: "suzanne collins",
+                pageCount: 2000,
+                language: "english",
+                published: new Date(2020, 1, 1),
+                desc:
+                    "Ambition will fuel him. Competition will drive him. But power has its price. It is the morning of the reaping that will kick off the tenth annual Hunger Games. In the Capitol, eighteen-year-old Coriolanus Snow is preparing for his one shot at glory as a mentor in the Games",
+            }
+        };
+    }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.bookInfoMain {
+    padding-top: 80px;
+}
+</style>
