3f91c2549daa0ea36e1f78e619f1588571a9a762
devnull03 <arnav2003mehta@gmail.com>
2021-06-25 17:18:40 +0530

    Made Cover its own component

---
+++ frontend/src/components/Cover.vue
@@ -0,0 +1,45 @@
+<template>
+    <img :src="imgUrl" class="cover" :style="cssVars(width, height)" />
+</template>
+
+<script>
+export default {
+    name: "Cover",
+    props: {
+        imgUrl: {
+            default: require("../assets/ProfilePicture.svg")
+        },
+        width: {
+            type: Number,
+            default: 250
+        },
+        height: {
+            type: Number,
+            default: 350
+        }
+    },
+    mounted() {
+        this.imgUrl = this.imgUrl? this.imgUrl: require("../assets/ProfilePicture.svg")
+    },
+    methods: {
+        cssVars: (width, height) => {
+            return {
+                '--width': width + 'px',
+                '--height': height + 'px',
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+.cover {
+    width: var(--width);
+    height: var(--height);
+    background: #181c23;
+    border: 3px solid #3d475c;
+    border-radius: 10px;
+    box-shadow: 0px 5px 10px 0px #00000080;
+    position: relative;
+}
+</style>
