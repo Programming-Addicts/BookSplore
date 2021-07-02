3bbf0c462b2359c7958aadedf336cd1bd453b440
devnull03 <arnav2003mehta@gmail.com>
2021-07-02 12:51:11 +0530

    fixed the new footer bug

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,45 @@
 <template>
-	<table style="width: 100%; height: 100%;">
-		<tr>
-			<router-view/>
-		</tr>
-		<tr>
-			<Footer v-if="$route.path != '/dashboard'" />
-		</tr>
-	</table>
+    <div class="outerMost">
+        <div class="r">
+            <router-view />
+        </div>
+        <div class="f">
+            <Footer v-if="$route.path != '/dashboard'" />
+        </div>
+    </div>
 </template>
 
 <script>
-
-import Footer from "@/components/Footer.vue"
+import Footer from "@/components/Footer.vue";
 
 export default {
-	name: "App",
-	components: {
-		Footer
-	},
-	methods: {
-		getHeight() {
-			return {
-				width: "100%",
-				height: `${window.innerHeight}px`,
-			}
-		}
-	}
+    name: "App",
+    components: {
+        Footer
+    },
+    methods: {
+        getHeight() {
+            return {
+                width: "100%",
+                height: `${window.innerHeight}px`
+            };
+        }
+    }
 };
 </script>
 
 <style scoped>
+.outerMost {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    justify-content: space-between;
+}
+.outerMost .f {
+    margin-top: auto;
+    position: relative;
+}
+.outerMost .r {
+    margin-bottom: auto;
+}
 </style>
