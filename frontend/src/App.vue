e2104d3f22ac85efc7df877cb22f0997ce90945d
PythonAddict <classpythonaddike@gmail.com>
2021-08-31 09:57:12 +0530

    Fix image url in embed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,22 @@
 <template>
-    <div>
-        <router-view />
-        <Footer
-            v-if="
-                $route.path != '/dashboard' && !$route.path.startsWith('/read')
-            "
-        />
-    </div>
+	<div>
+		<router-view />
+		<Footer
+			v-if="
+				$route.path != '/dashboard' && !$route.path.startsWith('/read')
+			"
+		/>
+	</div>
 </template>
 
 <script>
 import Footer from "./components/Footer.vue";
 
 export default {
-    name: "App",
-    components: {
-        Footer
-    }
+	name: "App",
+	components: {
+		Footer,
+	},
 };
 </script>
 
